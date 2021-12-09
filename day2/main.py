8efc81f33d4b3fd06b6c573dcad4479fd7bd8b03
Muhammad Dony Mulya <vveirt@gmail.com>
2021-12-09 13:56:17 +0800

    day 2 part 2

--- day2/main.py
+++ day2/main.py
@@ -4,21 +4,57 @@
 # down X increases the depth by X units.
 # up X decreases the depth by X units.
 
-horizontal = 0
-depth = 0
+
+def part_1():
+    horizontal = 0
+    depth = 0
+
+    with open("./day2.input") as f:
+        for line in f.readlines():
+            # split and convert into int
+            command, num = line.split()
+            num = int(num)
+
+            if command == "forward":
+                horizontal += num
+            elif command == "down":
+                depth += num
+            else:
+                depth -= num
+
+    print(horizontal * depth)
 
 
-with open("./day2.input") as f:
-    for line in f.readlines():
-        # split and convert into int
-        command, num = line.split()
-        num = int(num)
+# down X increases your aim by X units.
+# up X decreases your aim by X units.
+# forward X does two things:
+#     It increases your horizontal position by X units.
+#     It increases your depth by your aim multiplied by X.
 
-        if command == "forward":
-            horizontal += num
-        elif command == "down":
-            depth += num
-        else:
-            depth -= num
 
-print(horizontal * depth)
+def part_2():
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    with open("./day2.input") as f:
+        for line in f.readlines():
+            # split and convert into int
+            command, num = line.split()
+            num = int(num)
+
+            if command == "forward":
+                horizontal += num
+                if aim > 0:
+                    depth += num * aim
+            elif command == "down":
+                aim += num
+            else:
+                aim -= num
+
+    print(horizontal * depth)
+
+
+if __name__ == "__main__":
+    part_1()
+    part_2()
