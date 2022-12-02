09a90713fb9f7ddab8b302894076b5fc06efc9bb
Veirt <vveirt@gmail.com>
2022-12-02 16:10:47 +0800

    fix day 1 solution 2

--- day1/python/main.py
+++ day1/python/main.py
@@ -6,16 +6,17 @@ def solution_1():
     sum_list = []
     with open("./day1.input.txt", "r") as f:
         lines = [*f.readlines(), "\n"]
-        temp = 0
+        temp_max = 0
         for line in lines:
             if line != "\n":
-                temp += int(line)
+                temp_max += int(line)
             else:
-                max = temp if temp > max else max
-                sum_list.append(temp)
-                temp = 0
+                max = temp_max if temp_max > max else max
+                sum_list.append(temp_max)
+                temp_max = 0
 
     print(max)  # part 1
+    print((sorted(sum_list)[-3:]))  # part 2
     print(sum(sorted(sum_list)[-3:]))  # part 2
 
 
@@ -23,18 +24,24 @@ def solution_2():
     max_list = [0]
     with open("./day1.input.txt", "r") as f:
         lines = [*f.readlines(), "\n"]
-        temp = 0
+        temp_max = 0
         for line in lines:
             if line != "\n":
-                temp += int(line)
+                temp_max += int(line)
             else:
-                if temp > min(max_list):
-                    max_list.append(temp)
+                if temp_max > min(max_list):
+                    max_list.append(temp_max)
 
                 if len(max_list) > 3:
-                    max_list.pop(0)
+                    max_list.remove(min(max_list))
 
-                temp = 0
+                temp_max = 0
 
     print(max(max_list))  # part 1
-    print(max_list)  # part 2
+    print((max_list))  # part 2
+    print(sum(max_list))  # part 2
+
+
+solution_1()
+print()
+solution_2()
